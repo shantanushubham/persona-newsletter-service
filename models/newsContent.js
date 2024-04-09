83a08b4552d4f0d9995a020903613ffe27a48c3b
Shantanu Shubham <shantanu0707@gmail.com>
2024-04-09 17:34:47 +0530

    [Add] Basic DB calls

--- models/newsContent.js
+++ models/newsContent.js
@@ -16,6 +16,11 @@ const NewsContent = sequelize.define("news_content", {
     type: Sequelize.ARRAY(Sequelize.ENUM(...Object.values(TopicEnum))),
     allowNull: false,
   },
+  time: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+    allowNull: false,
+  },
 });
 
 module.exports = NewsContent;
---
+++ repository/user.repository.js
@@ -0,0 +1,36 @@
+const User = require("../models/user");
+
+const addUser = async (userData) => {
+  try {
+    const newUser = await User.create({
+      ...userData,
+    });
+    console.info(
+      `User with email: ${newUser.email} and subscribed topic ${newUser.topic} successfully`
+    );
+    return newUser;
+  } catch (error) {
+    console.error(`Error adding user:`, error);
+    throw error;
+  }
+};
+
+const getUsersBySubscribedTopics = async (topicList) => {
+  try {
+    const users = await User.findAll({
+      where: {
+        subscribedTopic: topicList,
+      },
+    });
+    console.info(`Found ${users.length} users by the given topics`);
+    return users;
+  } catch (error) {
+    console.error("Error getting users by subscribed topics:", error);
+    throw error;
+  }
+};
+
+module.exports = {
+  addUserToDb: addUser,
+  getUsersBySubscribedTopics,
+};
